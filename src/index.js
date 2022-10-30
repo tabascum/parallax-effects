1d2c75ac67b75f615620356a371585cd81911196
tabascum <marques_vasco@yahoo.es>
2022-10-30 17:21:41 +0000

    Re-build the project structure with Babel and Webpack

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles/index.css";
+
+//parallax effect function
+
+const parallax = document.getElementById("parallax");
+
+window.addEventListener("scroll", function () {
+  let offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset * 0.7 + "px";
+});
